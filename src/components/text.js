9feae711f4c43a4c28a083a31c0ac0faf44bd6dc
WenyaoWenyao <linwenyao007@gmail.com>
2022-06-19 09:41:01 +0000

    commit

---
+++ src/components/text.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const Text = ({text = '123', defaultParameter = 0 }) => {
+    const [counter, setCounter] = useState(defaultParameter);
+    const onChange = (increment) => {
+        setCounter(counter + increment);
+    }
+    return (
+        <div>
+            <p>{text}</p>
+            <h2>{counter}</h2>
+            <button onClick={ () => onChange(-1)}>decrement value</button>
+            <button onClick={ () => onChange(1)}>increment value</button>
+        </div>
+    )
+}
+
+export default Text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
